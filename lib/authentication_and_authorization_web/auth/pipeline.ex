27dc4d77dcbb2d89658162afc9902d2f8ad8b170
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-11-05 16:22:59 +0100

    Update pipeline.ex

--- lib/authentication_and_authorization_web/auth/pipeline.ex
+++ lib/authentication_and_authorization_web/auth/pipeline.ex
@@ -1,7 +1,8 @@
 defmodule AuthenticationAndAuthorizationWeb.Auth.Pipeline do
+  alias AuthenticationAndAuthorizationWeb.ErrorHandler
   use Guardian.Plug.Pipeline, otp_app: :authentication_and_authorization,
                               module: AuthenticationAndAuthorization.Guardian,
-                              error_handler: AuthenticationAndAuthorizationWeb.Auth.ErrorHandler
+                              error_handler: ErrorHandler
 
   plug Guardian.Plug.VerifyHeader
   plug Guardian.Plug.EnsureAuthenticated
